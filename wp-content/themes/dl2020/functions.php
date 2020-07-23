752bd6b1ae4c7e0b9ac51639a338e2f79fc2e64b
Don Livingston <donlivingston@gmail.com>
2020-07-23 15:05:30 -0600

    version bump in functions

--- wp-content/themes/dl2020/functions.php
+++ wp-content/themes/dl2020/functions.php
@@ -120,11 +120,11 @@ add_action( 'widgets_init', 'dl2020_widgets_init' );
  * Enqueue scripts and styles.
  */
 function dl2020_scripts() {
-	wp_enqueue_style( 'dl2020-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'dl2020-style', get_stylesheet_uri(), array(), '1.2.1', false );
 
-	wp_enqueue_style( 'dl2020-style-main', get_stylesheet_directory_uri(). '/main.css' );
+	wp_enqueue_style( 'dl2020-style-main', get_stylesheet_directory_uri(). '/main.css', array(), '1.2.1', false );
 
-	wp_enqueue_script( 'dl2020-index', get_template_directory_uri() . '/main.js', array(), '20200606', false );
+	wp_enqueue_script( 'dl2020-index', get_template_directory_uri() . '/main.js', array(), '1.2.1', false );
 
 	wp_enqueue_script( 'dl2020-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
